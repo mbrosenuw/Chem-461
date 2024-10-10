8c63b1dbc0fa22d015a23dcf01c541ae38af42c3
mbrosenuw <mbrosen@uw.edu>
2024-10-10 14:40:09 -0700

    making changes

--- main.py
+++ main.py
@@ -16,28 +16,28 @@ if os.path.exists(filename):
             specT = np.append(specT, float(row[1]))
 
 specA = -np.log10((specT+14)/np.max((specT+14)))
-# specA = specA/np.max(specA)
+specA = specA/np.max(specA)
 # plt.figure()
 # plt.plot(freq,specA)
 # plt.show()
 
 from uwpchem461 import Analyse
 analyse = Analyse()
-# fig, axs = plt.subplots(nrows=2, ncols=1, figsize=(10, 7))
+fig, axs = plt.subplots(nrows=1, ncols=1, figsize=(12,5))
 
 # # Plotting the dataset for specT vs wavenumber
 # axs[0].plot(freq, specT, linestyle='solid',
 #          marker='None', color='black', linewidth=1)
 # axs[0].set_xlabel('Energy [1/cm]')
 # axs[0].set_ylabel('Transmission [%]')
-#
-# # Plotting the dataset for absorbance vs wavenumber
-# axs[1].plot(freq, specA, linestyle='solid',
-#          marker='None', color='black', linewidth=1)
-# axs[1].set_xlabel('Energy [1/cm]')
-# axs[1].set_ylabel('Absorbance [Au]')
-# fig.suptitle('Rotation-Vibration Spectrum for HCl/DCl mixture')
-# plt.show()
+
+# Plotting the dataset for absorbance vs wavenumber
+axs.plot(freq, specA, linestyle='solid',
+         marker='None', color='black', linewidth=1)
+axs.set_xlabel('Energy [1/cm]')
+axs.set_ylabel('Absorbance [Au]')
+fig.suptitle('Rotation-Vibration Spectrum for HCl/DCl mixture')
+plt.show()
 
 cDClfundp = (freq > 1900) & (freq < 2250)
 cDCloverp = (freq > 3950) & (freq < 4250)
@@ -65,27 +65,31 @@ bHCLf = analyse.getbase(HClfund.tolist(), 0.075)
 bHCLo = analyse.getbase(HClover.tolist(), 0.1)
 
 
-fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 7))
-axs[0,0].plot(DClfundp[:,0], DClfundp[:,1], label = 'DCl Fundamental')
-axs[0,0].plot(DClfund[:,0], bDCLf, label = 'baseline')
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12,9))
+axs[0,0].plot(DClfundp[:,0], DClfundp[:,1], label = 'Signal')
+axs[0,0].plot(DClfund[:,0], bDCLf, label = 'Baseline')
 axs[0,0].set_title('DCl Fundamental')
 axs[0,0].set_xlabel('Energy [cm^-1]')
 axs[0,0].set_ylabel('Abs [Au]')
-axs[1,0].plot(DCloverp[:,0], DCloverp[:,1], label = 'DCl Overtone')
-axs[1,0].plot(DClover[:,0], bDCLo, label = 'baseline')
+axs[0,0].legend()
+axs[1,0].plot(DCloverp[:,0], DCloverp[:,1], label = 'Signal')
+axs[1,0].plot(DClover[:,0], bDCLo, label = 'Baseline')
 axs[1,0].set_title('DCl Overtone')
 axs[1,0].set_xlabel('Energy [cm^-1]')
 axs[1,0].set_ylabel('Abs [Au]')
-axs[0,1].plot(HClfundp[:,0], HClfundp[:,1], label = 'HCL Fundamental')
-axs[0,1].plot(HClfund[:,0], bHCLf, label = 'baseline')
+axs[1,0].legend()
+axs[0,1].plot(HClfundp[:,0], HClfundp[:,1], label = 'Signal')
+axs[0,1].plot(HClfund[:,0], bHCLf, label = 'Baseline')
 axs[0,1].set_title('HCl Fundamental')
 axs[0,1].set_xlabel('Energy [cm^-1]')
 axs[0,1].set_ylabel('Abs [Au]')
-axs[1,1].plot(HCloverp[:,0], HCloverp[:,1], label='HCl Overtone')
-axs[1,1].plot(HClover[:,0], bHCLo, label ='baseline')
+axs[0,1].legend()
+axs[1,1].plot(HCloverp[:,0], HCloverp[:,1], label = 'Signal')
+axs[1,1].plot(HClover[:,0], bHCLo, label ='Vaseline')
 axs[1,1].set_title('HCl Overtone')
 axs[1,1].set_xlabel('Energy [cm^-1]')
 axs[1,1].set_ylabel('Abs [Au]')
+axs[1,1].legend()
 fig.suptitle('HCl and DCl Bands')
 plt.subplots_adjust(top=0.88, hspace=0.45, wspace=0.4)
 plt.show()
@@ -140,46 +144,46 @@ h35or = h35o[h35o[:,0] > 5667, :]
 h37op = h37o[h37o[:,0] < 5667, :]
 h37or = h37o[h37o[:,0] > 5667, :]
 
-# fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 7))
-# axs[0,0].plot(DClfund[:,0], DClfund[:,1])
-# axs[0,0].scatter(d35fp[:,0], d35fp[:,1], label = 'DCl(35) P', color = 'red', marker = 's')
-# axs[0,0].scatter(d35fr[:,0], d35fr[:,1], label = 'DCl(35) R', color = 'orange', marker = 'o')
-# axs[0,0].scatter(d37fp[:,0], d37fp[:,1], label = 'DCl(37) P', color = 'green', marker = '^')
-# axs[0,0].scatter(d37fr[:,0], d37fr[:,1], label = 'DCl(37) R', color = 'purple', marker = '*')
-# axs[0,0].set_title('DCl Fundamental')
-# axs[0,0].set_xlabel('Energy [cm^-1]')
-# axs[0,0].set_ylabel('Abs [Au]')
-# axs[0,0].legend(loc = 'best')
-# axs[1,0].plot(DClover[:,0], DClover[:,1])
-# axs[1,0].scatter(d35op[:,0], d35op[:,1], label = 'DCl(35) P', color = 'red', marker = 's')
-# axs[1,0].scatter(d35or[:,0], d35or[:,1], label = 'DCl(35) R', color = 'orange', marker = 'o')
-# axs[1,0].scatter(d37op[:,0], d37op[:,1], label = 'DCl(37) P', color = 'green', marker = '^')
-# axs[1,0].scatter(d37or[:,0], d37or[:,1], label = 'DCl(37) R', color = 'purple', marker = '*')
-# axs[1,0].set_title('DCl Overtone')
-# axs[1,0].set_xlabel('Energy [cm^-1]')
-# axs[1,0].set_ylabel('Abs [Au]')
-# axs[1,0].legend(loc = 'best')
-# axs[0,1].plot(HClfund[:,0], HClfund[:,1])
-# axs[0,1].scatter(h35fp[:,0], h35fp[:,1], label = 'HCl(35) P', color = 'red', marker = 's')
-# axs[0,1].scatter(h35fr[:,0], h35fr[:,1], label = 'HCl(35) R', color = 'orange', marker = 'o')
-# axs[0,1].scatter(h37fp[:,0], h37fp[:,1], label = 'HCl(37) P', color = 'green', marker = '^')
-# axs[0,1].scatter(h37fr[:,0], h37fr[:,1], label = 'HCl(37) R', color = 'purple', marker = '*')
-# axs[0,1].set_title('HCl Fundamental')
-# axs[0,1].set_xlabel('Energy [cm^-1]')
-# axs[0,1].set_ylabel('Abs [Au]')
-# axs[0,1].legend(loc = 'best')
-# axs[1,1].plot(HClover[:,0], HClover[:,1])
-# axs[1,1].scatter(h35op[:,0], h35op[:,1], label = 'HCl(35) P', color = 'red', marker = 's')
-# axs[1,1].scatter(h35or[:,0], h35or[:,1], label = 'HCl(35) R', color = 'orange', marker = 'o')
-# axs[1,1].scatter(h37op[:,0], h37op[:,1], label = 'HCl(37) P', color = 'green', marker = '^')
-# axs[1,1].scatter(h37or[:,0], h37or[:,1], label = 'HCl(37) R', color = 'purple', marker = '*')
-# axs[1,1].set_title('HCl Overtone')
-# axs[1,1].set_xlabel('Energy [cm^-1]')
-# axs[1,1].set_ylabel('Abs [Au]')
-# axs[1,1].legend(loc = 'best')
-# fig.suptitle('Corrected HCl and DCl Bands')
-# plt.subplots_adjust(top=0.88, hspace=0.45, wspace=0.4)
-# plt.show()
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12,9))
+axs[0,0].plot(DClfund[:,0], DClfund[:,1], zorder = 1)
+axs[0,0].scatter(d35fp[:,0], d35fp[:,1], label = 'DCl(35) P', color = 'red', marker = 's')
+axs[0,0].scatter(d35fr[:,0], d35fr[:,1], label = 'DCl(35) R', color = 'orange', marker = 'o')
+axs[0,0].scatter(d37fp[:,0], d37fp[:,1], label = 'DCl(37) P', color = 'green', marker = '^')
+axs[0,0].scatter(d37fr[:,0], d37fr[:,1], label = 'DCl(37) R', color = 'purple', marker = '*')
+axs[0,0].set_title('DCl Fundamental')
+axs[0,0].set_xlabel('Energy [cm^-1]')
+axs[0,0].set_ylabel('Abs [Au]')
+axs[0,0].legend(loc = 'best')
+axs[1,0].plot(DClover[:,0], DClover[:,1], zorder = 1)
+axs[1,0].scatter(d35op[:,0], d35op[:,1], label = 'DCl(35) P', color = 'red', marker = 's')
+axs[1,0].scatter(d35or[:,0], d35or[:,1], label = 'DCl(35) R', color = 'orange', marker = 'o')
+axs[1,0].scatter(d37op[:,0], d37op[:,1], label = 'DCl(37) P', color = 'green', marker = '^')
+axs[1,0].scatter(d37or[:,0], d37or[:,1], label = 'DCl(37) R', color = 'purple', marker = '*')
+axs[1,0].set_title('DCl Overtone')
+axs[1,0].set_xlabel('Energy [cm^-1]')
+axs[1,0].set_ylabel('Abs [Au]')
+axs[1,0].legend(loc = 'best')
+axs[0,1].plot(HClfund[:,0], HClfund[:,1], zorder = 1)
+axs[0,1].scatter(h35fp[:,0], h35fp[:,1], label = 'HCl(35) P', color = 'red', marker = 's')
+axs[0,1].scatter(h35fr[:,0], h35fr[:,1], label = 'HCl(35) R', color = 'orange', marker = 'o')
+axs[0,1].scatter(h37fp[:,0], h37fp[:,1], label = 'HCl(37) P', color = 'green', marker = '^')
+axs[0,1].scatter(h37fr[:,0], h37fr[:,1], label = 'HCl(37) R', color = 'purple', marker = '*')
+axs[0,1].set_title('HCl Fundamental')
+axs[0,1].set_xlabel('Energy [cm^-1]')
+axs[0,1].set_ylabel('Abs [Au]')
+axs[0,1].legend(loc = 'best')
+axs[1,1].plot(HClover[:,0], HClover[:,1], zorder = 1)
+axs[1,1].scatter(h35op[:,0], h35op[:,1], label = 'HCl(35) P', color = 'red', marker = 's')
+axs[1,1].scatter(h35or[:,0], h35or[:,1], label = 'HCl(35) R', color = 'orange', marker = 'o')
+axs[1,1].scatter(h37op[:,0], h37op[:,1], label = 'HCl(37) P', color = 'green', marker = '^')
+axs[1,1].scatter(h37or[:,0], h37or[:,1], label = 'HCl(37) R', color = 'purple', marker = '*')
+axs[1,1].set_title('HCl Overtone')
+axs[1,1].set_xlabel('Energy [cm^-1]')
+axs[1,1].set_ylabel('Abs [Au]')
+axs[1,1].legend(loc = 'best')
+fig.suptitle('Corrected HCl and DCl Bands')
+plt.subplots_adjust(top=0.88, hspace=0.45, wspace=0.4)
+plt.show()
 
 def label(p, r):
     p = p[p[:,0].argsort()[::-1]]
@@ -187,46 +191,34 @@ def label(p, r):
     r = r[r[:, 0].argsort()]
     rarr = np.hstack((r, np.arange(1, r.shape[0] + 1).reshape(-1, 1)))
     out = np.vstack((parr, rarr))
-    return out[out[:, 2].argsort()]
+    out = out[out[:, 2].argsort()]
+    poly, cov = np.polyfit(out[:, 2], out[:, 0], deg=2, cov=True)
+    err = np.sqrt(np.diag(cov))
+    return out, poly, err
 
-h35fl = label(h35fp, h35fr)
-h35fpoly, residuals, _, _, _ = np.polyfit(h35fl[:,2], h35fl[:,0], deg = 2, full= True)
-h35fpolyerr = (residuals[0] if len(residuals) > 0 else 0)/h35fl.shape[0]
 
-h37fl = label(h37fp, h37fr)
-h37fpoly, residuals, _, _, _ = np.polyfit(h37fl[:,2], h37fl[:,0], deg = 2, full= True)
-h37fpolyerr = (residuals[0] if len(residuals) > 0 else 0)/h37fl.shape[0]
+h35fl, h35fpoly, h35ferrs = label(h35fp, h35fr)
+h37fl, h37fpoly, h37ferrs = label(h37fp, h37fr)
+d35fl, d35fpoly, d35ferrs = label(d35fp, d35fr)
+d37fl, d37fpoly, d37ferrs = label(d37fp, d37fr)
+h35ol, h35opoly, h35oerrs = label(h35op, h35or)
+h37ol, h37opoly, h37oerrs = label(h37op, h37or)
+d35ol, d35opoly, d35oerrs = label(d35op, d35or)
+d37ol, d37opoly, d37oerrs = label(d37op, d37or)
 
-d35fl = label(d35fp, d35fr)
-d35fpoly, residuals, _, _, _ = np.polyfit(d35fl[:,2], d35fl[:,0], deg = 2, full= True)
-d35fpolyerr = (residuals[0] if len(residuals) > 0 else 0)/d35fl.shape[0]
+def geterr(errs, m):
+    return np.sqrt(errs[0]**2 + m**2*errs[1]**2 + (m**4)*errs[2]**2)
 
-d37fl = label(d37fp, d37fr)
-d37fpoly, residuals, _, _, _ = np.polyfit(d37fl[:,2], d37fl[:,0], deg = 2, full= True)
-d37fpolyerr = (residuals[0] if len(residuals) > 0 else 0)/d37fl.shape[0]
-
-h35ol = label(h35op, h35or)
-h35opoly, residuals, _, _, _ = np.polyfit(h35ol[:,2], h35ol[:,0], deg = 2, full= True)
-h35opolyerr = (residuals[0] if len(residuals) > 0 else 0)/h35ol.shape[0]
-
-h37ol = label(h37op, h37or)
-h37opoly, residuals, _, _, _ = np.polyfit(h37ol[:,2], h37ol[:,0], deg = 2, full= True)
-h37opolyerr = (residuals[0] if len(residuals) > 0 else 0)/h37ol.shape[0]
-
-d35ol = label(d35op, d35or)
-d35opoly, residuals, _, _, _ = np.polyfit(d35ol[:,2], d35ol[:,0], deg = 2, full= True)
-d35opolyerr = (residuals[0] if len(residuals) > 0 else 0)/d35ol.shape[0]
-
-d37ol = label(d37op, d37or)
-d37opoly, residuals, _, _, _ = np.polyfit(d37ol[:,2], d37ol[:,0], deg = 2, full= True)
-d37opolyerr = (residuals[0] if len(residuals) > 0 else 0)/d37ol.shape[0]
-
-mval = np.linspace(-10,10,100)
-fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(10, 7))
+mval = np.linspace(-10,10,21)
+fig, axs = plt.subplots(nrows=2, ncols=2, figsize=(12, 9))
 axs[0,0].scatter(h35fl[:,2], h35fl[:,0], label = 'HCl(35) data', color='red', marker='o')
 axs[0,0].scatter(d35fl[:,2], d35fl[:,0], label = 'DCl(35) data', color='blue', marker='s')
 axs[0,0].plot(mval, np.polyval(h35fpoly, mval), label = 'HCl(35) fit', color='red')
 axs[0,0].plot(mval, np.polyval(d35fpoly, mval), label = 'DCl(35) fit', color='blue')
+axs[0,0].errorbar(mval, np.polyval(h35fpoly, mval), yerr=5*geterr(h35ferrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
+axs[0,0].errorbar(mval, np.polyval(d35fpoly, mval), yerr=5*geterr(d35ferrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
 axs[0,0].set_title('Cl(35) Fundamentals')
 axs[0,0].set_xlim([-10,10])
 axs[0,0].set_xlabel('m')
@@ -236,6 +228,10 @@ axs[1,0].scatter(h37fl[:,2], h37fl[:,0], label = 'HCl(37) data', color='red', ma
 axs[1,0].scatter(d37fl[:,2], d37fl[:,0], label = 'DCl(37) data', color='blue', marker='s')
 axs[1,0].plot(mval, np.polyval(h37fpoly, mval), label = 'HCl(37) fit', color='red')
 axs[1,0].plot(mval, np.polyval(d37fpoly, mval), label = 'DCl(37) fit', color='blue')
+axs[1,0].errorbar(mval, np.polyval(h35fpoly, mval), yerr=5*geterr(h37ferrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
+axs[1,0].errorbar(mval, np.polyval(d35fpoly, mval), yerr=5*geterr(d37ferrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
 axs[1,0].set_title('Cl(37) Fundamentals')
 axs[1,0].set_xlabel('m')
 axs[1,0].set_ylabel('Energy [cm^-1]')
@@ -245,6 +241,10 @@ axs[0,1].scatter(h35ol[:,2], h35ol[:,0], label = 'HCl(35) data', color='red', ma
 axs[0,1].scatter(d35ol[:,2], d35ol[:,0], label = 'DCl(35) data', color='blue', marker='s')
 axs[0,1].plot(mval, np.polyval(h35opoly, mval), label = 'HCl(35) fit', color='red')
 axs[0,1].plot(mval, np.polyval(d35opoly, mval), label = 'DCl(35) fit', color='blue')
+axs[0,1].errorbar(mval, np.polyval(h35opoly, mval), yerr=5*geterr(h35oerrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
+axs[0,1].errorbar(mval, np.polyval(d35opoly, mval), yerr=5*geterr(d35oerrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
 axs[0,1].set_title('Cl(37) Overtones')
 axs[0,1].set_xlabel('m')
 axs[0,1].set_ylabel('Energy [cm^-1]')
@@ -254,6 +254,10 @@ axs[1,1].scatter(h37ol[:,2], h37ol[:,0], label = 'HCl(37) data', color='red', ma
 axs[1,1].scatter(d37ol[:,2], d37ol[:,0], label = 'DCl(37) data', color='blue', marker='s')
 axs[1,1].plot(mval, np.polyval(h37opoly, mval), label = 'HCl(37) fit', color='red')
 axs[1,1].plot(mval, np.polyval(d37opoly, mval), label = 'DCl(37) fit', color='blue')
+axs[1,1].errorbar(mval, np.polyval(h37opoly, mval), yerr=5*geterr(h37oerrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
+axs[1,1].errorbar(mval, np.polyval(d37opoly, mval), yerr=5*geterr(d37oerrs, mval), fmt='none', color='black',
+                  ecolor='gray', elinewidth=2, capsize=0, label= '10x error')
 axs[1,1].set_title('Cl(37) Overtones')
 axs[1,1].set_xlabel('m')
 axs[1,1].set_ylabel('Energy [cm^-1]')
