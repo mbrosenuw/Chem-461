b1ed1bfe278449bf4f329da47c4fee2b08f53e11
mbrosenuw <165088962+mbrosenuw@users.noreply.github.com>
2024-11-15 22:25:23 -0800

    Lab 1 (#1)
    * lab data and opener
    
    * final code for now
    
    * added rate constants
    
    * arrhenius
    
    * finish lab1

---
+++ lab 1/maincode.py
@@ -0,0 +1,352 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from os.path import join as j
+from scipy.constants import physical_constants as pc
+from matplotlib.ticker import ScalarFormatter
+
+
+class Run():
+    def __init__(self, path, n240, n290, n322, start, stop, temp, cut=False, c322=0, r322=10):
+        self.path = path
+        self.n240 = n240
+        self.n290 = n290
+        self.n322 = n322
+        self.start = start
+        self.stop = stop
+        self.d240 = opentime(j(self.path, self.n240), ravg=True)
+        self.d290 = opentime(j(self.path, self.n290), ravg=True)
+        self.d322 = opentime(j(self.path, self.n322), ravg=True, w=r322)
+        self.temp = temp
+        self.cut = cut
+        self.c322 = c322
+
+    def plotData(self):
+        fig, axs = plt.subplots(figsize=(12, 4), ncols=3, nrows=1)
+        fig.subplots_adjust(wspace=0.3)
+        axs[0].plot(self.d240[:, 0], self.d240[:, 1])
+        axs[0].set_title(self.n240)
+        axs[0].set_title('240 nm Abs vs time ')
+        axs[0].set_xlabel('Time')
+        axs[0].set_ylabel('Abs')
+        axs[1].plot(self.d290[:, 0], self.d290[:, 1])
+        axs[1].set_title(self.n290)
+        axs[1].set_title('290 nm Abs vs time ')
+        axs[1].set_xlabel('Time')
+        axs[1].set_ylabel('Abs')
+        axs[2].plot(self.d322[:, 0], self.d322[:, 1])
+        axs[2].set_title(self.n322)
+        axs[2].set_title('322 nm Abs vs time ')
+        axs[2].set_xlabel('Time')
+        axs[2].set_ylabel('Abs')
+        fig.suptitle(f'Plots of Abs vs time for {self.temp}C', fontsize=14)
+        fig.tight_layout(rect=[0, 0, 1, 0.95])
+        plt.show()
+
+    def fitdata(self):
+        fig, axs = plt.subplots(figsize=(12, 4), ncols=3, nrows=1)
+        fig.subplots_adjust(wspace=0.3)
+        c = (self.d240[:, 0] > self.start) & (self.d240[:, 0] < self.stop)
+        self.Ai240, self.kTot240, self.sAi240, self.skTot240 = fitabs(self.d240[c, :], axs[0], name=self.n240,
+                                                                      runname=self.temp + '_240')
+        axs[0].set_title(self.n240)
+        axs[0].set_title('240 nm Abs vs time ')
+        axs[0].set_xlabel('Time')
+        axs[0].set_ylabel('Abs')
+        c = (self.d290[:, 0] > self.start) & (self.d290[:, 0] < self.stop)
+        self.Ai290, self.kTot290, self.sAi290, self.skTot290 = fitabs(self.d290[c, :], axs[1], name=self.n290,
+                                                                      runname=self.temp + '_290')
+        axs[1].set_title(self.n290)
+        axs[1].set_title('290 nm Abs vs time ')
+        axs[1].set_xlabel('Time')
+        axs[1].set_ylabel('Abs')
+        if self.cut:
+            c = (self.d322[:, 0] > self.c322)
+            self.d322[c, 1] = 0
+        c = (self.d322[:, 0] > self.start) & (self.d322[:, 0] < self.stop)
+        self.k3, self.sk3 = fit322(self.d322[c, :], axs[2], name=self.n322, runname=self.temp + '_322')
+        axs[2].set_title(self.n322)
+        axs[2].set_title('322 nm Abs vs time ')
+        axs[2].set_xlabel('Time')
+        axs[2].set_ylabel('Abs')
+        fig.suptitle(f'Fits of Abs vs time for {self.temp}C', fontsize=14)
+        fig.tight_layout(rect=[0, 0, 1, 0.95])
+        plt.show()
+
+    def getrates(self):
+        bp0 = 8 * 10 ** (-4) * 28 / 35
+        l = 1
+        params = np.array([[self.Ai240], [self.Ai290], [self.kTot240]])
+        mat = np.array([[l * bp0 / self.kTot240 * 5400, l * bp0 / self.kTot240 * 6500, l * bp0 / self.kTot240 * 800],
+                        [l * bp0 / self.kTot290 * 15800, 0, 0], [1, 1, 1]])
+        k1, k2, k4 = np.linalg.solve(mat, params).flatten()
+
+        Sy = np.array([[self.sAi240 ** 2, 0, 0], [0, self.sAi290 ** 2, 0], [0, 0, self.skTot240]])
+        Sk = np.linalg.inv(mat) @ Sy @ np.linalg.inv(mat).T
+        sk1, sk2, sk4 = np.sqrt(np.diag(Sk))
+        print(self.temp, ' & ', r4(k1), ' & ', r4(k2), ' & ', r4(self.k3), ' & ', r4(k4), ' & ', r4(self.kTot240),
+              ' & ', r4(sk1), ' & ', r4(sk2), ' & ', r4(self.sk3), ' & ', r4(sk4), ' & ', r4(self.skTot240), '\\\\')
+        return np.array([float(self.temp), k1, k2, self.k3, k4])
+
+
+def openspec(filename, ravg=False, w=5):
+    # open a file and return the contents of x-y data, with the option to do a 'nearest neighbors' average for y.
+    # filename - the file you want to open
+    # ravg - yes no to averaging
+    # w window width for averaging
+
+    # Initialize lists to store x and y data
+    x_data = []
+    y_data = []
+
+    # Open the file and read line by line
+    with open(filename, 'r') as file:
+        start_reading = False
+
+        for line in file:
+            # Check for the beginning of data section
+            if '>>>>>Begin Spectral Data<<<<<' in line:
+                start_reading = True
+                continue
+
+            # If in data section, split and store x and y values
+            if start_reading:
+                # Split line into x and y values
+                parts = line.split()
+                if len(parts) == 2:  # Ensure it's a data line
+                    x, y = map(float, parts[1:])
+                    x_data.append(x)
+                    y_data.append(y)
+        if ravg:
+            y_data = np.convolve(y_data, np.ones(w) / w, mode='same')
+    x_data = x_data - np.min(x_data)
+    return np.vstack((x_data, y_data)).T
+
+
+def opentime(filename, ravg=False, w=5):
+    # open a file and return the contents of x-y data, with the option to do a 'nearest neighbors' average for y.
+    # filename - the file you want to open
+    # ravg - yes no to averaging
+    # w window width for averaging
+
+    # Initialize lists to store x and y data
+    x_data = []
+    y_data = []
+
+    # Open the file and read line by line
+    with open(filename, 'r') as file:
+        start_reading = False
+
+        for line in file:
+            # Check for the beginning of data section
+            if '>>>>>Begin Spectral Data<<<<<' in line:
+                start_reading = True
+                continue
+
+            # If in data section, split and store x and y values
+            if start_reading:
+                # Split line into x and y values
+                parts = line.split()
+                if len(parts) == 3:  # Ensure it's a data line
+                    x, y = map(float, parts[1:])
+                    x_data.append(x)
+                    y_data.append(y)
+
+        y_data = np.array(y_data)
+        y_data[y_data < 0] = 0
+        if ravg:
+            y_data = np.convolve(y_data, np.ones(w) / w, mode='same')
+
+    x_data = (x_data - np.min(x_data)) / 1000
+    return np.vstack((x_data, y_data)).T
+
+
+def fitabs(run, ax, name, runname):
+    run[:, 0] = run[:, 0] - np.min(run[:, 0])
+    Ainf = np.max(run[:, 1]) + 0.01
+
+    def b15(run, Ainf):
+        yprime = np.log(Ainf - run[:, 1]) + 0.01
+        slope, intercept = np.polyfit(run[:, 0], yprime, 1)
+        dA = np.exp(intercept)
+        k = -slope
+        return dA, k
+
+    dA, k = b15(run, Ainf)
+
+    def model_func(t, Ainf, dA, k):
+        return Ainf - dA * np.exp(-k * t)
+
+    p0 = [Ainf, dA, k]
+    params, covariance = curve_fit(model_func, run[:, 0], run[:, 1], p0=p0)
+    param_errors = np.sqrt(np.diag(covariance))
+    # Extract the fitting parameters
+    Ainf, dA, k = params
+    sAinf, sdA, sk = param_errors
+
+    # Generate y values based on the fitted model
+    y_fit = model_func(run[:, 0], Ainf, dA, k)
+    resid = run[:, 1] - y_fit
+    std = np.std(resid)
+    num_points = 7
+    indices = np.linspace(0, len(run[:, 0]) - 1, num_points, dtype=int)
+    # Plot the original data and the fitted curve
+    ax.plot(run[:, 0], run[:, 1], label='Data', color='gray', linewidth=0.5)
+    ax.plot(run[:, 0], y_fit, label='Fit', color='blue', linewidth=2)
+    ax.errorbar(run[indices, 0][1:-1], y_fit[indices][1:-1], yerr=std, fmt='none', color='red', label='Error bars',
+                capsize=3)
+    equation_text = f'$y = {Ainf:.2f} - {dA:.2f} e^{{-{k:.4f} t}}$'
+    ax.text(0.05, -0.25, equation_text, transform=ax.transAxes, fontsize=9, verticalalignment='top', color='black')
+    ax.set_title(name)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Abs')
+    ax.legend()
+    # print(runname, ' & ', r6(Ainf), ' & ', r6(dA), ' & ', r6(k), ' & ', r6(sAinf), ' & ', r6(sdA),
+    # ' & ', r6(sk), '\\\\')
+    return Ainf, k, sAinf, sk
+
+
+def fit322(run, ax, name, runname):
+    run[:, 0] = run[:, 0] - np.min(run[:, 0])
+    run[:, 1] = run[:, 1] - np.min(run[:, 1])
+
+    def model_func2(t, B, D, k3, kT):
+        return B * (1 - np.exp(-kT * t)) - D * (np.exp(-k3 * t) - np.exp(-kT * t))
+
+    p0 = [0.05, 0.05, 0.3, 0.03]
+    bounds = (0, [1, 1, 0.5, 0.1])
+    model_func2(run[:, 0], 0.05, 0.05, 0.3, 0.03)
+    params, covariance = curve_fit(model_func2, run[:, 0], run[:, 1], p0=p0, bounds=bounds)
+    param_errors = np.sqrt(np.diag(covariance))
+    # Extract the fitting parameters
+    B, D, k3, kT = params
+    sB, sD, sk3, skT = param_errors
+
+    # Generate y values based on the fitted model
+    y_fit = model_func2(run[:, 0], B, D, k3, kT)
+    resid = run[:, 1] - y_fit
+    std = np.std(resid)
+    num_points = 7
+    indices = np.linspace(0, len(run[:, 0]) - 1, num_points, dtype=int)
+    ax.plot(run[:, 0], run[:, 1], label='Data', color='gray', linewidth=0.5)
+    ax.plot(run[:, 0], y_fit, label='Fit', color='blue', linewidth=2)
+    ax.errorbar(run[indices, 0][1:-1], y_fit[indices][1:-1], yerr=std, fmt='none', color='red', label='Error bars',
+                capsize=3)
+    equation_text = f'$y = {B:.2f}(1-e^{{-{kT:.4f} t}}) - {D:.2f}(e^{{-{k3:.4f} t}}-e^{{-{kT:.4f} t}})$'
+    ax.text(0.05, -0.25, equation_text, transform=ax.transAxes, fontsize=9, verticalalignment='top', color='black')
+    ax.set_title(name)
+    ax.set_xlabel('Time')
+    ax.set_ylabel('Abs')
+    ax.legend()
+    # print(runname, ' & ', r6(B), ' & ', r6(D), ' & ', r6(k3), ' & ', r6(kT), ' & ', r6(sB), ' & ', r6(sD),
+    #       ' & ', r6(sk3), ' & ', r6(skT), '\\\\')
+    return k3, sk3
+
+
+def r6(arr):
+    def round_single_value(val):
+        if val == 0:
+            return 0
+        else:
+            return np.round(val, decimals=5 - int(np.floor(np.log10(np.abs(val)))))
+
+    vectorized_round = np.vectorize(round_single_value)
+    return vectorized_round(arr)
+
+
+def r4(arr):
+    def round_single_value(val):
+        if val == 0:
+            return 0
+        else:
+            return np.round(val, decimals=3 - int(np.floor(np.log10(np.abs(val)))))
+
+    vectorized_round = np.vectorize(round_single_value)
+    return vectorized_round(arr)
+
+
+r28_1_1 = Run('lab1 data/timeresolved/28_1', '28_1_r1_240nm.txt', '28_1_r1_290nm.txt', '28_1_r1_322nm.txt',
+              start=180, stop=700, temp='28.1')
+r28_1_2 = Run('lab1 data/timeresolved/28_1', '28_1_r2_240nm.txt', '28_1_r2_290nm.txt', '28_1_r2_322nm.txt',
+              start=123, stop=500, temp='28.1')
+r28_1_3 = Run('lab1 data/timeresolved/28_1', '28_1_r3_240nm.txt', '28_1_r3_290nm.txt', '28_1_r3_322nm.txt',
+              start=72, stop=750, temp='28.1', cut=False, c322=181, r322=100)
+r18_6_r1 = Run('lab1 data/timeresolved/18_6', '18_6_r1_240nm.txt', '18_6_r1_290nm.txt', '18_6_r1_322nm.txt',
+               start=120, stop=750, temp='18.6')
+# r18_6_r2 = Run('lab1 data/timeresolved/18_6', '18_6_r2_240nm.txt', '18_6_r2_290nm.txt', '18_6_r2_322nm.txt',
+#                start = 120, stop =750, temp='18.6')
+r23_0_r1 = Run('lab1 data/timeresolved/23_0', '23_0_r1_240nm.txt', '23_0_r1_290nm.txt', '23_0_r1_322nm.txt',
+               start=140, stop=1110, temp='23.0')
+r33_4_r1 = Run('lab1 data/timeresolved/33_4', '33_4_r1_240nm.txt', '33_4_r1_290nm.txt', '33_4_r1_322nm.txt',
+               start=90, stop=630, temp='33.4')
+
+data = [r18_6_r1, r23_0_r1, r28_1_1, r28_1_2, r28_1_3, r33_4_r1]
+
+runrates = []
+for r in data:
+    # r.plotData()
+    r.fitdata()
+
+    runrates.append(r.getrates())
+
+runrates = np.array(runrates)
+np.save('runrates.npy', runrates)
+
+runrates = np.load('runrates.npy')
+
+
+def arrhenius(rates):
+    tvec = rates[:, 0]
+    N = pc['Avogadro constant'][0]
+    k = pc['Boltzmann constant'][0]
+    xvec = 1 / (N * k * (tvec + 273.15))
+    k1, k2, k3 = rates[:, 1], rates[:, 2], rates[:, 3]
+
+    fig, axs = plt.subplots(nrows=1, ncols=3, figsize=(14, 5))
+
+    # Subplot function to reduce code repetition
+    def plot_subplot(ax, x, y, title, ylabel, equation_label):
+        ax.scatter(x, np.log(y), c='k')
+        ax.set_title(title, fontsize=16)
+        ax.set_ylabel(ylabel, fontsize=14)
+        ax.set_xlabel('$\\frac{1}{N_Ak_BT}$', fontsize=14)
+
+        coeffs, cov = np.polyfit(x, np.log(y), 1, cov=True)
+        slope, intercept = coeffs
+        ax.plot(x, np.polyval([slope, intercept], x), 'r-', label='Fitted line')
+
+        # Dynamic equation placement
+        equation_text = f'${equation_label} = {intercept:.2f} + {slope:.2f} \\frac{{1}}{{N_Ak_BT}}$'
+        ax.text(0.95, 0.95, equation_text, transform=ax.transAxes, fontsize=14,
+                verticalalignment='top', horizontalalignment='right')
+
+        return cov, intercept, slope
+
+    # Plot all subplots
+    cov1, intercept1, slope1 = plot_subplot(axs[0], xvec, k1, '$\\ln{k_1}$ vs $\\frac{1}{N_Ak_BT}$', '$\\ln{k_1}$',
+                                            '\\ln{k_1}')
+    cov2, intercept2, slope2 = plot_subplot(axs[1], xvec, k2, '$\\ln{k_2}$ vs $\\frac{1}{N_Ak_BT}$', '$\\ln{k_2}$',
+                                            '\\ln{k_2}')
+    cov3, intercept3, slope3 = plot_subplot(axs[2], xvec, k3, '$\\ln{k_3}$ vs $\\frac{1}{N_Ak_BT}$', '$\\ln{k_3}$',
+                                            '\\ln{k_3}')
+
+    # Apply scientific notation to all axes
+    for ax in axs.flat:
+        ax.xaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.yaxis.set_major_formatter(ScalarFormatter(useMathText=True))
+        ax.ticklabel_format(style='sci', scilimits=(0, 0), axis='both')
+
+    fig.suptitle('Plots of $k_i$ vs $\\frac{1}{N_Ak_BT}$', fontsize=22)
+    fig.tight_layout()
+    plt.show()
+
+    # Print results for LaTeX table
+    for i, (slope, intercept, cov) in enumerate(
+            [(slope1, intercept1, cov1), (slope2, intercept2, cov2), (slope3, intercept3, cov3)], 1):
+        berr, merr = np.sqrt(np.diag(cov))
+        Aerr = np.exp(berr) * berr
+        print(f'k_{i} & {r4(np.exp(intercept))} & {r4(-slope)} \\\\')
+        print(f'k_{i} errors & {r4(Aerr)} & {r4(merr)} \\\\')
+
+
+arrhenius(runrates)
---
+++ lab 1/uwpchem461.py
@@ -0,0 +1,232 @@
+import math
+import numpy as np
+import pandas as pd
+import scipy as sp
+
+
+class Analyse():
+    """analyse dataset from chem 461"""
+
+    def __init__(self):
+        """Initializing"""
+
+    def getbase(self, ds, hibar, nfitpts=30, adjust=25):
+        """
+        determine a baseline from experiment 42 data
+        Input:
+        ds - n by 2 array, data of absorbance vs wavenumber
+        hibar - scalar, height-value below which a baseline is interpolated
+        nfitpts - scalar, number of points used to interpolate a baseline
+        Output:
+        baseline = n by 1 array
+        """
+        ## parse dataset
+        dsarr = np.array(ds)
+        x = dsarr[:, 0]
+        y = dsarr[:, 1]
+        npts = len(x)
+        ## define a uniform increment, dN, across the length of the dataset
+        dN = npts/nfitpts
+        K = np.linspace(0.5, (nfitpts - 0.5), nfitpts)*dN
+        K = K.round().astype(int)
+        ## determine the sign of the derivative across the dataset
+        KI = K[1:-1]
+        dK = np.full(len(KI), dN/adjust)
+        dK = dK.round().astype(int)
+        MD = np.sign(y[KI + dK] - y[KI])
+        MD = np.append([-1], MD)
+        MD = np.append(MD, [1])
+        MD = MD.astype(int)
+        ## nudge the y-values downards until they are below at hibar
+        nudgefac = np.rint(0.0004*npts)
+        if (nudgefac < 1): nudgefac = 1
+        idx = np.where(y[K] > hibar)[0]
+        while(idx.size):
+            ## if y is increasing then move in the reverse direction
+            ## so use MD to determine the slope of y
+            K[idx] = K[idx] - MD[idx]*nudgefac
+            idx = np.where(y[K] > hibar)[0]
+
+        ## now keep nudging the y-values until a little upwards
+        idx = np.linspace(0, nfitpts)[0]
+        while(idx.size):
+            KN = K - MD*nudgefac
+            KN = KN.round().astype(int)
+            idx = np.where((y[KN] - y[K]) < 0)[0];
+            K[idx] = KN[idx]
+
+        ## now use the good indexes, which represents the fit points
+        ## that are spread out uniformly across the dataset and below hibar, 
+        ## to interpolate a baseline
+        xk = np.flip(x[K])  # arrange values in increasing sequence
+        yk = np.flip(y[K])
+        yb = sp.interpolate.CubicSpline(xk, yk)
+        baseline = yb(x)
+
+        return baseline
+
+    @staticmethod
+    def caloexp(time, rate_heatgain, rate_heatloss, rate_reaction, DeltaT,
+        tstart=145, Tstart=291, Troom=293):
+        """
+        The function is defined for scipy's fitting procedure in order to
+        optimized the temperature rise T as a function of time, t
+        This function models the temperature rise of the Paar Calorimeter in
+        experiment 3:
+        dT/dt = rate_heatgain - rate_heatloss*(T - Troom) + rate_reaction*T_rxn
+            = rate_heatgain - rate_heatloss*(T - Troom) 
+            + rate_reaction*DeltaT*exp(-rate_reaction*(t - tstart))
+        Input:
+        time - n by 1 array [sec]
+        parameters to optimize:
+        rate_heatgain - rate of heat gain from the stirrer [K/sec]
+        rate_heatloss - the rate of heat loss [1/sec]
+        rate_reaction - rate of heat gain due to reaction [1/sec]
+            heat is on from t=0  to  t= 1/(rate_reaction/2)
+        DeltaT - net rise of temperature due to chemical reaction [K]
+        e.g. of guess parameters to optimize: pars = [0.003 0.001 0.02 5]
+        default paremeters as karg
+        tstart=30 - begining of reaction (heat of combution) [sec]
+        Tstart=292 - the first temperature in the data [K]
+        Troom=290 - temperature of the reservoir (or room) [K]
+        Output:
+        Tcurve - n by 1 array, the temperature range [K]
+        """
+        # Set up the heating region that goes from tstart 
+        idheat = [i for i, j in enumerate(time) if j >= tstart]
+        Zt = [time[i] - tstart for i in idheat]
+        # The temperature in the Zt range from start to end
+        TZ = [DeltaT*rate_reaction*(math.exp(-i*rate_heatloss) -
+           math.exp(-i*rate_reaction))/(rate_reaction - rate_heatloss) 
+           for i in Zt]
+        Tmod = [0] * len(time)
+        for i, j in zip(idheat, TZ):
+            Tmod[i] = j
+    
+        # the heat in and heat loss term is added to the Tmod 
+        # (due to reaction heating)
+        Eloss = [math.exp(-i*rate_heatloss) for i in time]
+        Zloss = []
+        if(rate_heatloss < 1e-7):  # take the limit if user sets rate_loss=0
+            Zloss = time
+        else:
+            Zloss = [(1 - i)/rate_heatloss for i in Eloss]
+    
+        TI = [i*rate_heatgain + j*Tstart + (1 - j)*Troom
+            for i, j in zip(Zloss, Eloss)]
+        Tcurve = [i + j for i, j in zip(TI, Tmod)]
+    
+        return Tcurve
+
+    def rst_exp9(self, var_pars, FHP):
+        """
+        A function to simulate the liquid-vapor phase-diagram for two liquids
+        Using Regular Solution Theory (RST) for the data from Exp 9, Chem 461
+        Needed: the boiling temperature (K) and the Heat of Vaporization
+        (KJ/mol) of two liquids A and B
+        Input:
+        FHP - FHP is the Flory Huggins (attraction) parameter must be set.
+          Set FHP = 0 to give a phase diagram for an ideal solution (Raoult's Law).
+          For Regular Solution Theory, the particular value for FHP depends on the molecules you use. 
+          For the alcohols in this Experiment 9, FHP is a value somewhere in the range of about -1 to 1.
+          You will make a few guesses to find a value of FHP that works with your data.
+        vap_pars - the parameters of vaporization, 
+            pars = [TboilA, enthalpy_vapA, TboilB, enthalpy_vapB]
+            TboilA - ref. boiling point of pure A (preferably measured at 1 atm) [Units of Kelvin]
+            TboilB - ref. boiling point of pure B
+            enthalpy_vapA - heat of vaporization of pure A [KJ/mol]
+            enthalpy_vapB - heat of vaporization of pure B
+            eg:  var_pars  =  [383.8, 33.18, 390.6, 43.29];
+        Output:
+          xB - the mole fraction of B in the solution
+          yB - the mole fraction of B in vapor phase 
+          Tbest - the equilibrium boiling temperature at which 
+            xB and yB are determined
+        Note: The total pressure, Ptot, is set to 1 atm, but can be changed.
+        """
+        # constants
+        npts = 200;
+        xB = np.linspace(0, 1, npts)
+        xA = 1 - xB                   # This just says the mole factions add to 1.
+        R = 8.314                     # Units of gas constant are J/(mol K)
+        TboilA = var_pars[0]; enthalpy_vapA = var_pars[1]*1e3/R/TboilA;
+        TboilB = var_pars[2]; enthalpy_vapB = var_pars[3]*1e3/R/TboilB;
+        PboilA = 1; PboilB = 1;       # Units of P are atm.
+        P0 = 1; T0 = 360; Ptot = 1;   # Units of T are K. Units of P are atm.
+
+        # The Clausius Clapeyron Relation
+        # You may be most familiar with this equation from textbooks
+        # in the form P*/Pref = exp [(-deltaH/R)(1/T - 1/Tref).
+        PA_pure = lambda Tx : np.exp(enthalpy_vapA*(Tx - TboilA)/Tx)*P0
+        PB_pure = lambda Tx : np.exp(enthalpy_vapB*(Tx - TboilB)/Tx)*P0
+        # xA = (Ptot - PB_pure(T0))/(PA_pure(T0) - PB_pure(T0))
+        # yA = (xA*PA_pure(T0))/Ptot
+
+        # Using regular solution theory, we calculate the partial pressures.
+        # Find the equilibrium temperature for the liquid-vapor mole fractions.
+        # First, extend the temperature range to find the answer.
+        # The extended range should be ten times the magnitude of FHP.
+        pad = 10*np.rint(FHP);
+        range_T = np.array(
+            np.minimum(TboilA, TboilB) - pad, np.maximum(TboilA, TboilB) + pad
+            )
+        
+        # Now, the following lines of code optimize to find the best
+        # boiling temperature that fits the Regular Solution Theory equation
+        Tbest = np.zeros(npts)
+        for k in range(0, npts):
+            PA = lambda Tx : xA[k]*np.exp(FHP*(xB[k]**2))*PA_pure(Tx)
+            PB = lambda Tx : xB[k]*np.exp(FHP*(xA[k]**2))*PB_pure(Tx)
+            # The next two lines simply require Ptot = PA + PB
+            find_T = lambda Tx : PA(Tx) + PB(Tx) - Ptot       
+            Tbest[k] = sp.optimize.newton(find_T, range_T)
+
+        # The line below plugs in the best boiling temp into the RST equation
+        yB = xB*np.exp(FHP*(xA**2))*PB_pure(Tbest)/Ptot
+
+        return xB, yB, Tbest
+
+
+class Opener():
+    """read dataset from chem461"""
+
+    def __init__(self):
+       """Initializing""" 
+
+    def getdata(self, dnfn):
+        """
+        method to read text/csv data using pandas' package
+        Input:
+        dnfn - string, path to file
+        Output:
+        data - array, data array
+        """
+        # the dataframe of pandas
+        df = pd.read_csv(dnfn)
+        # convert dataframe into numpy array
+        ds = df.to_numpy()
+
+        return ds
+
+    def get_txt_data(self, dnfn, headerlines=2):
+        """
+        method to read the cvs data for exp 42
+        """
+        ds = []
+ 
+        with open(dnfn, 'rt') as f:
+ 
+            # skip header lines
+            for i in range(headerlines):
+                next(f)
+ 
+            # readline and convert string to number array
+            for line in f:
+                linetrunc_x = line[0:6].strip()
+                linetrunc_y = line[8:].strip()
+                # convert string array to number array
+                data = [float(linetrunc_x), float(linetrunc_y)]
+                data_abs = [data[0], -np.log10(data[1]/100)] 
+                ds.append(data_abs)
+ 
+        return ds, dnfn
